0571067e6ea2f966ab1f3a684ce5a8cd456ded7e
jubayer-ahmed-ratul <ahmedjubayer.ratul@gmail.com>
2026-05-14 11:45:26 +0600

    add body parsers and create POST endpoint for user data

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,38 @@
-import express from "express"
-const app = express()
-const port = 5000
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
 
-app.get('/user', (req, res) => {
-  res.send('server is running!')
-})
+const app: Application = express();
+const port = 5000;
+
+app.use(express.json()); //middlewire
+app.use(express.text()); //middlewire
+app.use(express.urlencoded({ extended: true }));
+
+//GET METHOD
+app.get("/", (req: Request, res: Response) => {
+  // res.send("server is running!");
+  res.status(200).json({
+    message: "Server is running",
+    author: "Jubayer",
+  });
+});
+
+//POST METHOD
+app.post("/", async (req: Request, res: Response) => {
+  // console.log(req.body);
+  const { name, email, password } = req.body;
+  res.status(201).json({
+    message: "Created",
+    data: {
+      name,
+      email,
+    },
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
