b0f60fd837614b3e93a762268eb10b2eb437836c
jubayer-ahmed-ratul <ahmedjubayer.ratul@gmail.com>
2026-05-14 12:40:52 +0600

    connect to neon and created query

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,41 @@ import express, {
   type Response,
 } from "express";
 
+import { Pool } from "pg";
 const app: Application = express();
 const port = 5000;
 
 app.use(express.json()); //middlewire
 app.use(express.text()); //middlewire
 app.use(express.urlencoded({ extended: true }));
+
+const pool = new Pool({
+  connectionString:
+    "postgresql://neondb_owner:npg_pkBgUX60SwDO@ep-wild-sunset-aqrd20vc-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+});
+
+const initDB = async () => {
+  try {
+    await pool.query(`
+      
+      CREATE TABLE IF NOT EXISTS users(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(20),
+      email VARCHAR(20) NOT NULL,
+      password VARCHAR(20) NOT NULL,
+      is_active BOOLEAN DEFAULT true,
+      age INT,
+
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+    )
+      `);
+       console.log("database connected successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+initDB();
 
 //GET METHOD
 app.get("/", (req: Request, res: Response) => {
@@ -23,6 +52,7 @@ app.get("/", (req: Request, res: Response) => {
 //POST METHOD
 app.post("/", async (req: Request, res: Response) => {
   // console.log(req.body);
+
   const { name, email, password } = req.body;
   res.status(201).json({
     message: "Created",
